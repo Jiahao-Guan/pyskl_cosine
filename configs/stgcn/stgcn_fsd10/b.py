3d79e1938a0ace95bfb5d90b328dc6ea9a88465b
Jiahao Guan <64345765+Jiahao-Guan@users.noreply.github.com>
2024-09-04 14:16:15 +0800

    Update b.py

--- configs/stgcn/stgcn_fsd10/b.py
+++ configs/stgcn/stgcn_fsd10/b.py
@@ -6,11 +6,10 @@ model = dict(
     cls_head=dict(type='GCNHead', num_classes=10, in_channels=256))
 
 dataset_type = 'PoseDataset'
-# 制作数据集
 ann_file = 'data/fsd10/fsd10.pkl'
 train_pipeline = [
     #dict(type='PreNormalize2D'),
-    dict(type='GenSkeFeat', dataset='openpose_25', feats=['b']), # 生成特征，根据feats中的字段选择生成joint或bone以及motion
+    dict(type='GenSkeFeat', dataset='openpose_25', feats=['b']), 
     dict(type='UniformSample', clip_len=100),
     dict(type='PoseDecode'),
     dict(type='FormatGCNInput', num_person=2),
